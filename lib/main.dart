0573d212fb8e6be9997bba06f788ec7a4c13b824
ANAΓKH <ma674359954@icloud.com>
2020-07-14 11:28:19 +0800

    begin

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
+import 'alibipage.dart';
+import 'homepage.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          child: new Text('Hello World'),
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+    ));
