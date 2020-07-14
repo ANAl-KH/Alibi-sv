6184ef34611ec0b449559f1252d7b92c19b3db58
ANAΓKH <ma674359954@icloud.com>
2020-07-14 17:04:24 +0800

    map

---
+++ lib/alibipage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AlibiPage extends StatefulWidget {
+  @override
+  _AlibiPageState createState() => _AlibiPageState();
+}
+
+class _AlibiPageState extends State<AlibiPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
