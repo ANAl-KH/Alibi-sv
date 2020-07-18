51207a8da0c804ea849de970dad6572ffb65e78d
ANAΓKH <ma674359954@icloud.com>
2020-07-18 10:56:24 +0800

    Update resjsonmodel.dart

--- lib/resjsonmodel.dart
+++ lib/resjsonmodel.dart
@@ -1,3 +1,14 @@
+//解析打点钱包接口返回的json所需要的类
+/*打点钱包返回的json示例：
+{
+    "code": 0,
+    "msg": "ok (Req-Id:5f779582-4027-f6c9-3a68-79deb15f30d8)",
+    "data": {
+        "fee": 134,
+        "order_id": "31edd664-d8da-4b30-881d-37a282ac6059",
+        "tx_id": "9cd7c669a257f65f16ed2499e3960dbbf8cb3cae2c17c9384549c0b97686693e"
+    }
+}*/
 class Dotdata {
   int fee;
   String orderid;
